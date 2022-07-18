2850a46f02f2787741ff5da7f28020d75820fc4a
Pedro Padilha Neto <pedropadilhan@gmail.com>
2022-07-18 12:29:58 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,12 @@
-function App(){
-  return(
-    <h1>Olá mundo!</h1>
+import NotificationButton from "./components/NotificationButton"
+
+function App() {
+  return (
+    <>
+      <h1>Olá mundo!</h1>
+      <NotificationButton />
+      <NotificationButton />
+    </>
   )
 }
 
