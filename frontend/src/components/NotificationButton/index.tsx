6c0acc340b5a44fe2985e467f0b5a4057c66dcb7
Pedro Padilha Neto <pedropadilhan@gmail.com>
2022-09-06 11:12:03 -0300

    useState

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,6 +1,6 @@
-import icon from '../../assets/img/notification-icon.svg'
+import icon from '../../assets/img/notification-icon.svg';
 
-import './styles.css'
+import './styles.css';
 
 function NotificationButton() {
     return (
@@ -13,4 +13,4 @@ function NotificationButton() {
     )
 }
 
-export default NotificationButton
+export default NotificationButton;
